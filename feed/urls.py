73b01c1022af8e3ad5c78a499f4e34652db096a3
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2025-08-31 14:43:00 +0200

    create a detailView

--- feed/urls.py
+++ feed/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import HomeView
+from .views import PostDetailView
 
 app_name='feed'   
 
 urlpatterns=[
     path('',HomeView.as_view(),name='index'),
-    ]  
+    path('post/<int:pk>',PostDetailView.as_view(),name='post_details')     
+    ] 
--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView   
 from .models import Post
+from django.views.generic import DetailView 
 
 
 class HomeView(TemplateView):
@@ -8,6 +9,10 @@ class HomeView(TemplateView):
     def get_context_data(self,**kwargs):
         context=super().get_context_data(**kwargs)
         context['posts']=Post.objects.all()
-
-        
+  
         return context
+    
+class PostDetailView(DetailView):
+    model=Post
+    template_name="details.html"
+    context_object_name="post"
